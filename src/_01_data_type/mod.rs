08a551737fb00fd51539968f62c9064678ee1d45
Kewal Khondekar <kewalkhondekar25@gmail.com>
2025-12-02 18:38:02 +0530

    feat: data types added

--- src/_01_data_type/mod.rs
+++ src/_01_data_type/mod.rs
@@ -2,3 +2,26 @@
 pub fn sum(a: u32, b: u32) -> u32 {
     return a + b;
 }
+
+//boolean
+pub fn is_even(num: u32) -> bool {
+    return  num % 2 == 0;
+}
+
+//strings
+pub fn print_string() -> String {
+    let name: String = String::from("kewal");
+    return name;
+}
+
+//array
+pub fn print_array() -> [u32; 5] {
+    let arr: [u32; 5] = [1, 2, 3, 4, 5]; 
+    return arr;
+}
+
+//vector
+pub fn print_vector() -> Vec<u32>{
+    let vec: Vec<u32> = vec![69, 69, 69];
+    return vec;
+}
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,11 @@ mod _01_data_type;
 
 fn main() {
     println!("Hello, world!");
+    
+    println!("-DataTypes-");
     println!("{}", _01_data_type::sum(69, 69));
-}
+    println!("{}", _01_data_type::is_even(69));
+    println!("{}", _01_data_type::print_string());
+    println!("{:?}", _01_data_type::print_vector());
+    println!("{:?}", _01_data_type::print_array());
+}   
