cfd123be808f9b5f05161c6ae86874b5a7cc0a4a
Kewal Khondekar <kewalkhondekar25@gmail.com>
2025-12-02 07:44:57 +0530

    feat: numbers added

---
+++ src/_01_data_type/mod.rs
@@ -0,0 +1,4 @@
+//numbers
+pub fn sum(a: u32, b: u32) -> u32 {
+    return a + b;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod _01_data_type;
+
 fn main() {
     println!("Hello, world!");
+    println!("{}", _01_data_type::sum(69, 69));
 }
