f2f01c9d8b7cb414f85413d0b9323f9a5c063575
Kewal Khondekar <kewalkhondekar25@gmail.com>
2025-12-06 00:33:03 +0530

    feat: struct methods added

--- src/_08_structs/mod.rs
+++ src/_08_structs/mod.rs
@@ -3,6 +3,18 @@ struct Rect {
     width: u32
 }
 
+impl Rect {
+    //method
+    fn area(&self) -> u32 {
+        return self.height * self.width;
+    }
+
+    //static method
+    fn get_rect(){
+        println!("this is static method")
+    }
+}
+
 pub fn print_struct(){
     
     let r = Rect {
@@ -10,5 +22,8 @@ pub fn print_struct(){
         width: 20
     };
 
-    println!("{}, {}", r.height, r.width)
+    println!("{}, {}", r.height, r.width);
+    println!("{}", r.area());
+    //call static method
+    Rect::get_rect();
 }
